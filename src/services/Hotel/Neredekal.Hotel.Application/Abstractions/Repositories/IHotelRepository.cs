7adfb3eb080a9d9f6cf015082a6171a3b4e13108
Sefa ÖZ <sefa_oz@hotmail.com>
2024-10-21 16:00:13 +0300

    Hotel Application Katmanı Oluşturulması

---
+++ src/services/Hotel/Neredekal.Hotel.Application/Abstractions/Repositories/IHotelRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neredekal.Common.Domain.SeedWorks;
+
+namespace Neredekal.Hotel.Application.Abstractions.Repositories
+{
+    public interface IHotelRepository : IUnitOfWorks
+    {
+        Task Create(Domain.AggregateModels.HotelModels.Hotel hotel);
+        Task Delete(Domain.AggregateModels.HotelModels.Hotel hotel);
+        Task<Domain.AggregateModels.HotelModels.Hotel> Get(Guid id);
+    }
+}
---
+++ src/services/Hotel/Neredekal.Hotel.Application/Wrappers/Result.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neredekal.Hotel.Application.Wrappers
+{
+    public class Result
+    {
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+
+        public Result(string message, bool isSuccess = true)
+        {
+            Message = message;
+            IsSuccess = isSuccess;
+        }
+
+        public static Result Success(string message)
+        {
+            return new(message);
+        }
+
+        public static Result Error(string message)
+        {
+            return new(message, false);
+        }
+    }
+
+    public class Result<T> where T : class
+    {
+        public T Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+
+        public Result(T data, string message, bool isSuccess = true)
+        {
+            Data = data;
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+
+        public static Result<T> Success(T data, string message)
+        {
+            return new(data, message);
+        }
+
+        public static Result<T> Error(T data, string message)
+        {
+            return new(data, message, false);
+        }
+    }
+}
