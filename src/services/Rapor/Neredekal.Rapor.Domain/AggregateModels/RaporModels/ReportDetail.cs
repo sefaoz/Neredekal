af59ca5294fb1af2fd9244ae4e0b19cfca0817ea
Sefa ÖZ <sefa_oz@hotmail.com>
2024-10-21 16:15:39 +0300

    Rapor Domain Katmanı Oluşturulması.

---
+++ src/services/Rapor/Neredekal.Rapor.Domain/AggregateModels/RaporModels/ReportDetail.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neredekal.Common.Domain.SeedWorks;
+
+namespace Neredekal.Rapor.Domain.AggregateModels.RaporModels
+{
+    public class ReportDetail : AggregateRoot
+    {
+        #region MyRegion
+        public override Guid UUID { get; set; }
+        public DateTime RequestDate { get; set; }
+        public ReportStatusEnum ReportStatus { get; set; }
+        public string Data { get; set; }
+        #endregion
+
+        #region ctor
+        protected ReportDetail() { }
+
+        private ReportDetail(Guid id, ReportStatusEnum reportStatus, string data)
+        {
+            UUID = id;
+            ReportStatus = reportStatus;
+            Data = data;
+            RequestDate = DateTime.UtcNow;
+        }
+        #endregion
+
+        #region methods
+
+        public static ReportDetail CreateReportDetail(Guid id, string data, ReportStatusEnum reportStatus = ReportStatusEnum.Preparing)
+        {
+            return new(id, reportStatus, data);
+        }
+
+        public void SetStatusCompleted()
+        {
+            ReportStatus = ReportStatusEnum.Completed;
+        }
+
+        #endregion
+    }
+}
---
+++ src/services/Rapor/Neredekal.Rapor.Domain/AggregateModels/RaporModels/ReportStatusEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neredekal.Rapor.Domain.AggregateModels.RaporModels
+{
+    public enum ReportStatusEnum
+    {
+        Preparing = 1,
+        Completed =2
+    }
+}
