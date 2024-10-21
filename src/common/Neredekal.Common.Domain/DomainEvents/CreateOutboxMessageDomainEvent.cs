d3cb5b9a607f35c2fc8f6ceda437d4dedb931b30
Sefa ÖZ <sefa_oz@hotmail.com>
2024-10-21 12:32:09 +0300

    Domain Event ve Outbox Model Oluşturulması.

---
+++ src/common/Neredekal.Common.Domain/DomainEvents/CreateOutboxMessageDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neredekal.Common.Domain.SeedWorks;
+
+namespace Neredekal.Common.Domain.DomainEvents
+{
+    public record CreateOutboxMessageDomainEvent(string Type, string Content) : IDomainEvent;
+}
---
+++ src/common/Neredekal.Common.Domain/OutboxModel/OutboxMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neredekal.Common.Domain.OutboxModel
+{
+    public class OutboxMessage
+    {
+        public Guid Id { get; init; }
+        public string Type { get; private set; } = string.Empty;
+        public string Content { get; private set; } = string.Empty;
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? ProcessedAt { get; private set; }
+        public string? Error { get;private set; }
+        public OutboxMessageStatus Status { get; private set; }
+
+        protected OutboxMessage() { }
+
+        private OutboxMessage(Guid id, string type, string content, OutboxMessageStatus status)
+        {
+            Id = id;
+            Type= type;
+            Content = content;
+            Status = status;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public static OutboxMessage Create(Guid id, string type, string content,
+            OutboxMessageStatus status = OutboxMessageStatus.Pending)
+        {
+            return new OutboxMessage(id, type, content, status);
+        }
+
+        public void SetStatusPublished()
+        {
+            Status = OutboxMessageStatus.Published;
+            ProcessedAt = DateTime.UtcNow;
+        }
+
+        public void SetStatusError(string error)
+        {
+            Status = OutboxMessageStatus.Error;
+            Error = error;
+            ProcessedAt = DateTime.UtcNow;
+        }
+    }
+}
