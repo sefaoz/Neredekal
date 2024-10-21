87bccb8920428acf94182388127aef50d43953c7
Sefa ÖZ <sefa_oz@hotmail.com>
2024-10-21 16:03:00 +0300

    Hotel Api Katmanı Oluşturulması

---
+++ src/services/Hotel/Neredekal.Hotel.Api/Controllers/HotelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Neredekal.Hotel.Api.Controllers
+{
+    public class HotelController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
