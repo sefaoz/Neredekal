73a29ad01f30f4a112bdc0214f6fe4d0a612d31f
Sefa ÖZ <sefa_oz@hotmail.com>
2024-10-21 16:50:28 +0300

    Rapor Api Katmanı Oluşturulması

---
+++ src/services/Rapor/Neredekal.Rapor.Api/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Neredekal.Rapor.Api.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
